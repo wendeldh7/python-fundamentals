b616a5fcda7d6beef0b31abd9d3effb0b578fa8e
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-04-29 18:24:07 -0400

    feat: Lesson on data types in python

---
+++ 01 - Tipos De Dados/tipos_de_dados.py
@@ -0,0 +1,5 @@
+print(1 + 10 + 100) # Soma de números inteiros / tip int
+print(1.5 + 1 + 100.5) # Soma de números decimais /tip float
+print(True) # Booleano / tip bool
+print(False) # Booleano / tip bool
+print("python") # String / tip str
