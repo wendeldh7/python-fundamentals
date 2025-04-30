0ceef78f6a3cd678bc7f875d407232f382205dae
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-04-30 11:47:09 -0400

    feat: Continuation of the class on conditional / nested / ternary structures in python

---
+++ 06 - Estruturas Condicionais e de Repetição em Python/estrutura_condicionais_ternaria.py
@@ -0,0 +1,21 @@
+# Estrutura Condicional Ternária
+
+# A estrutura condicional ternária é uma forma compacta de escrever uma condição if-else em Python. Ela permite que você atribua um valor a uma variável com base em uma condição, tudo em uma única linha.
+
+# Exemplo:
+
+saldo = 2000
+saque = 1500
+
+status = "Sucesso" if saldo >= saque else "Falha"
+
+print(f"{status} ao realizar o saque!")
+
+# Exercício para fixação de conteúdo
+
+saldo = 2000
+saque = 2500
+
+status = "Sucesso" if saldo >= saque else "Falha"
+
+print(f"{status} ao realizar o saque!")
