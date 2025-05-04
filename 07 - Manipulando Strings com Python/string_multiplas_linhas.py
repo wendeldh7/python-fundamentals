6667ba44de17ebf4192557fef5209d75d1bfbf2f
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-04 09:45:45 -0400

    feat: Continuation of the class manipulating strings with python / string multiple lines

---
+++ 07 - Manipulando Strings com Python/string_multiplas_linhas.py
@@ -0,0 +1,17 @@
+# Introdução
+
+# Strings são sequências de caracteres, e podem ser manipuladas de várias formas em Python.
+
+# Uma das formas de criar strings é utilizando aspas simples ou duplas, mas também podemos usar três aspas simples ou duplas para criar strings que ocupam várias linhas.
+
+# Isso é útil quando queremos criar strings longas ou formatadas de maneira mais legível.
+
+# Strings múltiplas linhas
+
+nome = "Wendel"
+
+mensagem = f"""Olá, meu nome é {nome},
+Eu estou aprendendo Python.
+""" # Essa string ocupa várias linhas e mantém a formatação.
+
+# A string acima é criada usando três aspas duplas, mas poderíamos usar três aspas simples também.
