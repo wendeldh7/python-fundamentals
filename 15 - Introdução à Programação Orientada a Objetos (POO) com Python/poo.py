3c1fd103ce3b3dccd21588b57144ba744815bf7b
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-10 09:19:43 -0400

    feat: Introduction to Object-Oriented Programming (OOP) with Python

---
+++ 15 - Introdução à Programação Orientada a Objetos (POO) com Python/poo.py
@@ -0,0 +1,21 @@
+# Programação orientada a objetos (POO)
+
+# Paradigmas de programação
+
+#  Um paradigma de programação é um estilo e programação Não é uma linguagem (Python, Java, C, etc), e sim a forma comovocê soluciona os problemas através do código.
+
+# Exemplo
+
+# Problema: Beber água
+# Solução 1: Usar um copo para beber água.
+# Solução 2: Usar uma garrafa para beber água.
+
+# Alguns paradigmas
+
+# Imperativo ou procedural
+# Funcional
+# Orientado a eventos
+
+# Programação orientada a objetos
+
+# O paradigma de programação orientada a objetos estrutura o código abstraindo problemas em objetos do mundo real facilitando o entendimento do código e tornando-o mais modular e extensível. Os dois conceitos chaves para aprender POO são: classes e objetos.
