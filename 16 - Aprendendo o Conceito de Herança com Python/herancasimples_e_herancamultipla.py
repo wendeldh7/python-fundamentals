123a8388f06634dd449ac14e733a09a4be029b28
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-12 13:32:04 -0400

    feat: Learning the Concept of Inheritance with Python / Single Inheritance and Multiple Inheritance

---
+++ 16 - Aprendendo o Conceito de Herança com Python/herancasimples_e_herancamultipla.py
@@ -0,0 +1,28 @@
+# Herança simples e herança múltipla
+
+# Herança simples
+
+#  Quando uma classe filha herda apenas uma classe pai, ela é chamada deherança simples.
+
+# Exemplo
+
+class A:
+    pass
+
+class B(A):
+    pass
+
+# Herança múltipla
+
+# Quando uma classe filha herda de várias classes pai, ela é chamada de herança múltipla.
+
+# Exemplo
+
+class A:
+    pass
+
+class B(A):
+    pass
+
+class C(A, B):
+    pass
