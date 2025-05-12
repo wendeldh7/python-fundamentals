9dd12c5981a94897f14381a7abb4132020ef3af0
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-12 13:23:18 -0400

    feat: Learning the Concept of Inheritance with Python / inheritance in python

---
+++ 16 - Aprendendo o Conceito de Herança com Python/heranca_em_poo.py
@@ -0,0 +1,19 @@
+# Herança em POO
+
+# O que é herança?
+
+#  Em programação herança é a capacidade de uma classe filha derivar ou herdar as características e comportamentos da classe pai (base).
+
+# Benefícios da herança
+
+# ● Representa bemosrelacionamentos do mundo real.
+ # ● *Fornece reutilização de código, não precisamos escrever o mesmo código repetidamente. Além disso, permite adicionar mais recursos a uma classe sem modificá-la.
+ # ● *Édenatureza transitiva, o que significa que, se a classe B herdar da classe A, todas as subclasses de B herdarão automaticamente da classe A.
+
+# Sintaxe da herança
+
+class A:
+    pass
+
+class B(A):
+    pass
