43d44b0c698dfa54065138a02cfc04d2177cc1c0
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-16 09:06:16 -0400

    feat: Interfaces and Abstract Classes with Python / Creating abstract classes

---
+++ 19 - Interfaces e Classes Abstratas com Python/classes_abstrata.py
@@ -0,0 +1,8 @@
+# Classes abstratas
+
+# O que são interfaces?
+# Interfaces definem o que uma classe deve fazer e não como.
+
+# Python tem interface?
+# O conceito de interface é definir um contrato, onde são declarados os métodos (o que deve ser feito) e suas respectivas assinaturas. Em Python utilizamos classes abstratas para criar contratos. Classes abstratas não podem ser instanciadas.
+
---
+++ 19 - Interfaces e Classes Abstratas com Python/modulo_abc.py
@@ -0,0 +1,61 @@
+# Criando classes abstratas com o módulo abc
+
+# ABC
+# Por padrão, o Python não fornece classes abstratas. O Python vem com um módulo que fornece abasepara definir as classes abstratas, e o nome do módulo é ABC. O ABC funciona decorando métodos da classe base como abstratos e, em seguida, registrando classes concretas como implementações da base abstrata. Um método se torna abstrato quando decorado com @abstractmethod.
+
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+    @property
+    @abstractproperty
+    def marca(self):
+        pass
+
+
+class ControleTV(ControleRemoto):
+    def ligar(self):
+        print("Ligando a TV...")
+        print("Ligada!")
+
+    def desligar(self):
+        print("Desligando a TV...")
+        print("Desligada!")
+
+    @property
+    def marca(self):
+        return "Philco"
+
+
+class ControleArCondicionado(ControleRemoto):
+    def ligar(self):
+        print("Ligando o Ar Condicionado...")
+        print("Ligado!")
+
+    def desligar(self):
+        print("Desligando o Ar Condicionado...")
+        print("Desligado!")
+
+    @property
+    def marca(self):
+        return "LG"
+
+
+controle = ControleTV()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
+
+
+controle = ControleArCondicionado()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
