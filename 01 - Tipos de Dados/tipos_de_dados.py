ff0413f5f1fec6155f08f591ff9a0e583d0f9ef7
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-17 19:29:30 -0400

    feat: Add examples of data types in Portuguese

---
+++ 01 - Tipos de Dados/tipos_de_dados.py
@@ -0,0 +1,5 @@
+print(1 + 10 + 100)       # Soma de inteiros, resultado: 111 (tipo int)
+print(1.5 + 1 + 100.5)    # Soma com floats e int, resultado: 103.0 (tipo float)
+print(True)               # Valor booleano True
+print(False)              # Valor booleano False
+print("python")           # String literal
