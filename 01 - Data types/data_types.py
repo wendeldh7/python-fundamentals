8f8721b2fcbc76b84e69cdde33ff88108f223db4
Wendel Gomes <wendelgomesdh7@gmail.com>
2025-05-17 11:21:31 -0400

    feat: Add data types examples in English and remove previous file in Portuguese

---
+++ 01 - Data types/data_types.py
@@ -0,0 +1,5 @@
+print(1 + 10 + 100)  # Sum of integers / type int
+print(1.5 + 1 + 100.5)  # Sum of decimal numbers / type float
+print(True)  # Boolean / type bool
+print(False)  # Boolean / type bool
+print("python")  # String / type str
